b57ad0ab2dda32222f24872047929cf4a1d88a63
desmetr <rafael.de.smet@gmail.com>
2019-04-15 11:00:14 +0200

    fixed chatDB initialization

--- src/chat/__init__.py
+++ src/chat/__init__.py
@@ -1,12 +1,18 @@
 #initialize application
 
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 chatApp=Flask(__name__)
 chatApp.config['SECRET_KEY']='\xc8/3\xad\xf5\xe5\xf5,\xb1\x9ck\xab\xbc\xa5\x9a\xad9\xbf\x1dCm\xc2\xe2\xb4'
-chatApp.config['SQLALCHEMY_DATABASE_URI']='sqlite:///chatApp.db'
+chatApp.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' + os.path.join(basedir, 'chatApp.db')
 chatApp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 chatDB=SQLAlchemy(chatApp)
+migrate = Migrate(chatApp, chatDB)
 
 from chat import routes
--- src/chat/chatApp.py
+++ src/chat/chatApp.py
@@ -1,4 +1,9 @@
-from chat import chatApp
+from chat import chatApp, chatDB
+from chat.models import Chat
+
+@chatApp.shell_context_processor
+def make_shell_context():
+	return {'chatDB': chatDB, 'Chat': Chat}
 
 if __name__=='__main__':
     chatApp.run(debug=True)
