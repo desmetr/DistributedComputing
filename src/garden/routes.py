9b0c89160b6561b84afc20680158e80e737dc207
MacBookproHelp <sp.keerthanaa@gmail.com>
2019-05-05 17:54:26 +0200

    we can see the count of veggies and fruits

--- src/garden/routes.py
+++ src/garden/routes.py
@@ -2,9 +2,8 @@ from garden.models import Garden
 from flask import render_template, jsonify
 from sqlalchemy import func
 from garden import db
-from sqlalchemy.sql import label
+from sqlalchemy import func
 from garden import app
-
 @app.route("/")
 @app.route("/garden", methods=['GET' , 'POST'])
 def garden():
@@ -17,12 +16,18 @@ def vegetable(User_id):
     vegetableItems = []
     for row in db.session.query(Garden).filter_by(User_id=User_id):
         vegetableItems.append(row.vegetable)
-    return (render_template('vegetables.html', title="vegetables",  vegItems=vegetableItems))
+
+    vegetableCo = Garden.query.with_entities(Garden.vegetable,func.count(Garden.vegetable)).group_by(Garden.vegetable).filter(Garden.User_id == User_id).all()
+    print(vegetableCo)
+
+    return(render_template('vegetables.html',title="vegetables", vegCount = vegetableCo, vegItems = vegetableItems))
 
 @app.route("/garden/<User_id>/fruits", methods=['GET','POST'])
 def fruits(User_id):
     fruitItems = []
     for row in db.session.query(Garden).filter_by(User_id=User_id):
         fruitItems.append(row.fruits)
-    return (render_template('fruits.html', title="fruits", fruitItems=fruitItems))
+    fruitsCount = Garden.query.with_entities(Garden.fruits,func.count(Garden.fruits)).group_by(Garden.fruits).filter(Garden.User_id == User_id).all()
+    print(fruitsCount)
+    return (render_template('fruits.html', title="fruits",fruCount=fruitsCount, fruitItems=fruitItems))
 
