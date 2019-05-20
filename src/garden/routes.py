fb4f26c3de6daf7c53272bbf9fb7a4f59ab0dc66
desmetr <rafael.de.smet@gmail.com>
2019-05-20 10:31:22 +0200

    small fix garden

--- src/garden/routes.py
+++ src/garden/routes.py
@@ -9,6 +9,9 @@ from garden.forms import GroceryForm
 from garden import db
 from sqlalchemy import func
 from garden import app
+
+current_user_id = ""
+
 @app.route("/")
 @app.route("/garden", methods=['GET', 'POST'])
 def garden():
@@ -32,9 +35,12 @@ def herbs():
     herbCount = Garden.query.with_entities(Garden.herbs, Garden.Img_id, func.count(Garden.herbs)).group_by(Garden.herbs).all()
     return (render_template('herbs.html', title="herbs", herbCount=herbCount))
 
+# FOR KEERTHANA: change this to showVegetables that only shows the vegetables of a user and do the same for fruits and herbs
 #This is the garden page we will have bar for veg,herb,fruits for that user
-@app.route("/garden/getVegetables", methods=['GET','POST'])
-def getVegetables():
+@app.route("/garden/showVegetables", methods=['GET','POST'])
+def showVegetables():
+    global current_user_id
+
     current_user_id = request.cookies.get("currentSessionCookie")
     if current_user_id:
         # Get current user information
@@ -54,26 +60,68 @@ def getVegetables():
             return redirect(urlsConfig.URLS['login_url'])
     else:
         return redirect(urlsConfig.URLS['login_url'])
-'''
+
+@app.route("/garden/<User_id>/getVegetables", methods=['GET','POST'])
+def getVegetables(User_id):
+    global current_user_id
+
+    current_user_id = request.cookies.get("currentSessionCookie")
+    if current_user_id:
+        # Get current user information
+        current_user_response = requests.get(urlsConfig.URLS['single_user_url'] + str(current_user_id))
+        if current_user_response.status_code == 200:
+            vegetablesItems = []
+            for row in db.session.query(Garden).filter_by(User_id=User_id):
+                vegetablesItems.append(row.vegetable)
+            vegetablesCount = Garden.query.with_entities(Garden.vegetable, Garden.Img_id, func.count(Garden.vegetable)).group_by(Garden.vegetable).filter(Garden.User_id == User_id).all()
+            return jsonify(vegetablesItems)
+        else:
+            return redirect(urlsConfig.URLS['login_url'])
+    else:
+        return redirect(urlsConfig.URLS['login_url'])
+
 @app.route("/garden/<User_id>/getFruits", methods=['GET','POST'])
 def getFruits(User_id):
-    fruitsItems = []
-    for row in db.session.query(Garden).filter_by(User_id=User_id):
-        fruitsItems.append(row.fruits)
-    fruitsCount = Garden.query.with_entities(Garden.fruits, Garden.Img_id, func.count(Garden.fruits)).group_by(Garden.fruits).filter(Garden.User_id == User_id).all()
-    return jsonify(fruitsItems)
+    global current_user_id
+
+    current_user_id = request.cookies.get("currentSessionCookie")
+    if current_user_id:
+        # Get current user information
+        current_user_response = requests.get(urlsConfig.URLS['single_user_url'] + str(current_user_id))
+        if current_user_response.status_code == 200:
+            fruitsItems = []
+            for row in db.session.query(Garden).filter_by(User_id=User_id):
+                fruitsItems.append(row.fruits)
+            fruitsCount = Garden.query.with_entities(Garden.fruits, Garden.Img_id, func.count(Garden.fruits)).group_by(Garden.fruits).filter(Garden.User_id == User_id).all()
+            return jsonify(fruitsItems)
+        else:
+            return redirect(urlsConfig.URLS['login_url'])
+    else:
+        return redirect(urlsConfig.URLS['login_url'])
 
 @app.route("/garden/<User_id>/getHerbs", methods=['GET','POST'])
 def getHerbs(User_id):
-    herbsItems = []
-    for row in db.session.query(Garden).filter_by(User_id=User_id):
-        herbsItems.append(row.herbs)
-    herbsCount = Garden.query.with_entities(Garden.herbs, Garden.Img_id, func.count(Garden.herbs)).group_by(Garden.herbs).filter(Garden.User_id == User_id).all()
-    return jsonify(herbsItems)
+    global current_user_id
 
-'''
+    current_user_id = request.cookies.get("currentSessionCookie")
+    if current_user_id:
+        # Get current user information
+        current_user_response = requests.get(urlsConfig.URLS['single_user_url'] + str(current_user_id))
+        if current_user_response.status_code == 200:
+            herbsItems = []
+            for row in db.session.query(Garden).filter_by(User_id=User_id):
+                herbsItems.append(row.herbs)
+            herbsCount = Garden.query.with_entities(Garden.herbs, Garden.Img_id, func.count(Garden.herbs)).group_by(Garden.herbs).filter(Garden.User_id == User_id).all()
+            return jsonify(herbsItems)
+        else:
+            return redirect(urlsConfig.URLS['login_url'])
+    else:
+        return redirect(urlsConfig.URLS['login_url'])
+
 @app.route("/AddGrocery", methods=['GET', 'POST'])
 def AddGrocery():
+    global current_user_id
+
     current_user_id = request.cookies.get("currentSessionCookie")
     if current_user_id:
         # Get current user information
